d89159184b1e562290b3d61663c1bb7fd31c1c89
devbrunosanches <dev.brunosanches@gmail.com>
2023-06-22 17:32:24 -0300

    ai image generator

---
+++ server/routes/aiRoutes.js
@@ -0,0 +1,44 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import { Configuration, OpenAIApi } from 'openai';
+
+dotenv.config();
+
+const router = express.Router();
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration); // a payment method for openai api is necessary to make it work
+
+router.route('/').get((req, res) =>
+{
+    res.status(200).json({ message: 'AI Image Generator!' });
+});
+
+router.route('/').post(async (req, res) =>
+{
+    try {
+        const { prompt } = req.body;
+        console.log(req.body);
+
+        const aiResponse = await openai.createImage({
+            prompt: "",
+            n: 1,
+            size: "1024x1024",
+            //response_format: 'b64_json',
+        });
+        console.log(aiResponse.data);
+        
+        const data = await aiResponse.json();
+
+        const image = aiResponse.data.data[0].url;
+        res.status(200).json({ image });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send(error?.response.data.error.message);
+    }
+});
+
+export default router;
